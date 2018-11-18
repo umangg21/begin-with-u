2b3164120875ab0ec7a3854e4d9042102a10771c
umangg21 <umangg21@gmail.com>
2018-11-18 14:17:28 +0530

    Updates

---
+++ precache-manifest.fd812e00d9fa2f30a25b23eb08947f36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7083a434e45807a8e56",
+    "url": "/begin-with-u/static/js/runtime~main.c7083a43.js"
+  },
+  {
+    "revision": "c4b66275affc24f7d44c",
+    "url": "/begin-with-u/static/js/main.c4b66275.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/begin-with-u/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "c4b66275affc24f7d44c",
+    "url": "/begin-with-u/static/css/main.cdba0892.chunk.css"
+  },
+  {
+    "revision": "5145ccd4a8aaec6ce452be55ae183937",
+    "url": "/begin-with-u/index.html"
+  }
+];
