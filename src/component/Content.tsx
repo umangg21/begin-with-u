156e79aa684efab7489d895adcbe6e85d3ac4de4
umangg21 <umangg21@gmail.com>
2018-11-24 00:30:37 +0530

    Adding Dog's Language
    Bhow Bhow with Font Size and Family

--- src/component/Content.tsx
+++ src/component/Content.tsx
@@ -8,17 +8,26 @@ interface IContentStates {
 
 }
 
-function getContentStyle(){
-    return{
-        backgroundColor : `#263238`
+function getFontStyle() {
+    return {
+        color: `white`,
+        fontSize: 50,
+        fontFamily: 'forte',
+
+    }
+}
+
+function getContentStyle() {
+    return {
+        backgroundColor: `#263238`
     }
 }
 
 export class Content extends React.Component<IContentProps, IContentStates> {
     render() {
         return (
-            <div style={getContentStyle()}  className="flex-50 layout-row layout-align-center-center">
-                Content will appear here.
+            <div style={getContentStyle()} className="flex-50 layout-row layout-align-center-center">
+                <span style={getFontStyle()} > Bhow Bhow..</span>
             </div>
         )
     }
